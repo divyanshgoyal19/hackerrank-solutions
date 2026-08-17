5611898eb0eb9bd04e0b52251b1c1302b54ac403
Divyansh Goyal <dgoyal2_be23@thapar.edu>
2026-08-17 12:55:41 +0530

    ✅ Average Population [mysql] — approach1

---
+++ solutions/mysql/easy/average-population/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+-- Problem     Average Population
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-17, 12:55 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT FLOOR(avg(population)) from city;
