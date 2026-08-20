2df1c2feb2bfad1f78ce9da27582bd3638947a36
Divyansh Goyal <dgoyal2_be23@thapar.edu>
2026-08-20 18:45:44 +0530

    ✅ Revising Aggregations - Averages [mysql] — approach1

---
+++ solutions/mysql/easy/revising-aggregations-the-average-function/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true
+-- Problem     Revising Aggregations - Averages
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-08-20, 06:45 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT avg(population) from city where district = "california"
